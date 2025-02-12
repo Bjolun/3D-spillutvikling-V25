f9bc01611c874ce30f58494fc31fa8f0eafb68ba
Bjørn Arild Lunde <bjolun5@gmail.com>
2025-02-12 12:38:10 +0100

    Update PickUpCoins.cs

--- scripts/PickUpCoins.cs
+++ scripts/PickUpCoins.cs
@@ -13,8 +13,11 @@ public class PickUpCoins : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        // Spiller av lydklippet som hører til Audio Source'n vår
         audioSource.Play();
+        // Skru av MeshRenderer komponenten slik at det ser ut som myntene blir fjernet umiddelbart
         meshRenderer.enabled = false;
+        // Sletter dette game objektet en bestemt tid, i dette tilfelle etter lengden på lydklippet vårt.
         Destroy(gameObject, audioSource.clip.length);
     }
 }
