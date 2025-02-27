0b710697b9f8313fc8645331790050ebb78eac19
Bjørn Arild Lunde <bjolun5@gmail.com>
2025-02-27 10:02:46 +0100

    Script for å spille av animasjon

---
+++ scripts/PlayerAnimations.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    [SerializeField] PlayerMovement player;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (player.IsWalking())
+        {
+            animator.SetBool("isWalking", true);
+        } else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
