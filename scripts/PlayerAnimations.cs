05ad0e78bff1c50039c7a6357ca4571df149ef47
Bjørn Arild Lunde <bjolun5@gmail.com>
2025-02-27 10:49:44 +0100

    La inn kommentarer

--- scripts/PlayerAnimations.cs
+++ scripts/PlayerAnimations.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour
 {
+    // Referanse til spilleren vår slik at vi kan se etter input
     [SerializeField] PlayerMovement player;
 
+    // Trenger vi en referanse til Animator komponent
     private Animator animator;
 
     private void Start()
@@ -11,12 +13,16 @@ public class PlayerAnimations : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    // På hver frame ønsker vi å se om noe er trykket på
     private void Update()
     {
+        // Hvis WASD er trykket på, setter vi isWalking til true. (se PlayerMovement for å se IsWalking metoden)
         if (player.IsWalking())
         {
             animator.SetBool("isWalking", true);
-        } else
+        } 
+        // Hvis INGEN knapper er trykket på, så settes isWalking til false.
+        else
         {
             animator.SetBool("isWalking", false);
         }
