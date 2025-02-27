64541c34d76a5c17c1365f44ab9c5648e71e840b
Bjørn Arild Lunde <bjolun5@gmail.com>
2025-02-27 10:54:00 +0100

    La inn resten av koden vi skrev og kommentarer

--- scripts/PlayerMovement.cs
+++ scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    private bool isWalking = false;
 
     private void Update()
     {
@@ -29,9 +32,27 @@ public class PlayerMovement : MonoBehaviour
         inputVector = inputVector.normalized;
         Vector3 movementVector = new Vector3(inputVector.x, 0f, inputVector.y);
 
+        // Hvis verdien i movementVector er (0,0,0), altså ingen input, så settes isWalking til false
+        // Er den noe annet, altså noe input, så settes isWalking til true
+        if(movementVector == Vector3.zero)
+        {
+            isWalking = false;
+        }
+        else
+        {
+            isWalking = true;
+        }
+
         // Roterer karakteren vår
         transform.forward = Vector3.Slerp(transform.forward, movementVector, Time.deltaTime * rotationSpeed);
 
         transform.position += movementVector * movementSpeed * Time.deltaTime;
+    }
+
+    // Denne metoden er public, så den kan nås fra andre steder (denne brukes i PlayerAnimation).
+    // Returnerer isWalking.
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
